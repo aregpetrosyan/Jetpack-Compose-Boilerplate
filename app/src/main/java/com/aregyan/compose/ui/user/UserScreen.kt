eafb7d6456c5aaec8a753c9353d8312339f4422f
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-05-28 19:27:36 +0400

    Added LazyColumn

--- app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
+++ app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
@@ -1,6 +1,29 @@
 package com.aregyan.compose.ui.user
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aregyan.compose.ui.theme.JetpackComposeBoilerplateTheme
 
 @Composable
-fun UserScreen() {}
+fun UserScreen() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeBoilerplateTheme {
+        UserScreen()
+    }
+}
