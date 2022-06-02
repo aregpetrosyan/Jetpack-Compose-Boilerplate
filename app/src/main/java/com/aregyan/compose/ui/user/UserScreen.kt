283a96222874e4f689ac31c891495383d33096d1
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-06-02 10:34:27 +0400

    Improved UiState

--- app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
+++ app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
@@ -3,10 +3,9 @@ package com.aregyan.compose.ui.user
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -15,14 +14,14 @@ import com.aregyan.compose.ui.theme.JetpackComposeBoilerplateTheme
 @Composable
 fun UserScreen() {
     val viewModel = hiltViewModel<UserViewModel>()
-    val uiState by viewModel.uiState.collectAsState()
+    val uiState = viewModel.uiState
 
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.background)
     ) {
-
+        items(uiState.list) { item -> }
     }
 }
 
--- app/src/main/java/com/aregyan/compose/ui/user/UserViewModel.kt
+++ app/src/main/java/com/aregyan/compose/ui/user/UserViewModel.kt
@@ -1,37 +1,39 @@
 package com.aregyan.compose.ui.user
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aregyan.compose.domain.UserListItem
 import com.aregyan.compose.repository.UserListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class UserUiState(
+    val list: List<UserListItem> = listOf()
+)
 
 @HiltViewModel
 class UserViewModel @Inject constructor(
     private val userListRepository: UserListRepository
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(UserUiState.Success(emptyList()))
-    val uiState: StateFlow<UserUiState> = _uiState
+    var uiState by mutableStateOf(UserUiState())
+        private set
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
             userListRepository.refreshUserList()
             userListRepository.users.collect { list ->
-                _uiState.value = UserUiState.Success(list)
+                uiState = UserUiState(
+                    list = list
+                )
             }
         }
     }
 
-}
-
-sealed class UserUiState {
-    data class Success(val users: List<UserListItem>): UserUiState()
-    data class Error(val exception: Throwable): UserUiState()
 }
