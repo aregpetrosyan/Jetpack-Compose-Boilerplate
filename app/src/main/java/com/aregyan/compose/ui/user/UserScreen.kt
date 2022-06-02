b14f86e1940235558af19cc5e939c03ff9c2909a
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-06-02 14:43:11 +0400

    Added Coil

--- app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
+++ app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
@@ -1,17 +1,18 @@
 package com.aregyan.compose.ui.user
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.aregyan.compose.domain.UserListItem
 import com.aregyan.compose.ui.theme.JetpackComposeBoilerplateTheme
 
@@ -33,7 +34,17 @@ fun UserScreen() {
 
 @Composable
 fun UserItem(item: UserListItem) {
-    Text(text = item.username, modifier = Modifier.padding(vertical = 16.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            modifier = Modifier.padding(16.dp).size(40.dp),
+            model = item.avatar,
+            contentDescription = null
+        )
+        Text(text = item.username, modifier = Modifier.padding(horizontal = 16.dp))
+    }
 }
 
 @Preview(showBackground = true)
