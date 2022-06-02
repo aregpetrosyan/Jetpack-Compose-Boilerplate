cd7fc596fa4a9e4b0cc482c41e5158e4d256c3c0
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-06-02 10:53:53 +0400

    Added UserItem

--- app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
+++ app/src/main/java/com/aregyan/compose/ui/user/UserScreen.kt
@@ -2,13 +2,17 @@ package com.aregyan.compose.ui.user
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.aregyan.compose.domain.UserListItem
 import com.aregyan.compose.ui.theme.JetpackComposeBoilerplateTheme
 
 @Composable
@@ -21,8 +25,15 @@ fun UserScreen() {
             .fillMaxSize()
             .background(MaterialTheme.colors.background)
     ) {
-        items(uiState.list) { item -> }
+        items(uiState.list) { item -> 
+            UserItem(item = item)
+        }
     }
+}
+
+@Composable
+fun UserItem(item: UserListItem) {
+    Text(text = item.username, modifier = Modifier.padding(vertical = 16.dp))
 }
 
 @Preview(showBackground = true)
