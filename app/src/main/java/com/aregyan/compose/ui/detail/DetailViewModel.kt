d50ff01f14607863d4efdec997e617466201f8ae
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-06-04 12:42:06 +0400

    Added request

--- app/src/main/java/com/aregyan/compose/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/aregyan/compose/ui/detail/DetailViewModel.kt
@@ -1,9 +1,10 @@
 package com.aregyan.compose.ui.detail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.aregyan.compose.domain.UserDetails
 import com.aregyan.compose.repository.UserDetailsRepository
+import com.aregyan.compose.ui.Argument
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,15 +12,22 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    private val userDetailsRepository: UserDetailsRepository
+    private val userDetailsRepository: UserDetailsRepository,
+    savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
+
+    private val username: String? = savedStateHandle[Argument.USERNAME]
 
 //    val userDetails = ObservableParcelable(UserDetails())
 
     fun getUserDetails(user: String) = userDetailsRepository.getUserDetails(user)
 
-    fun refreshUserDetails(user: String) = viewModelScope.launch(Dispatchers.IO) {
-        userDetailsRepository.refreshUserDetails(user)
+    init {
+        username?.let {
+            viewModelScope.launch(Dispatchers.IO) {
+                userDetailsRepository.refreshUserDetails(it)
+            }
+        }
     }
 
 }
