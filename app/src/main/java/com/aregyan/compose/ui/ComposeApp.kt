441f67c234c791ef19a3c78ccd4fa81077f03488
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-06-04 12:08:45 +0400

    Added navigation improvement

--- app/src/main/java/com/aregyan/compose/ui/ComposeApp.kt
+++ app/src/main/java/com/aregyan/compose/ui/ComposeApp.kt
@@ -1,27 +1,46 @@
 package com.aregyan.compose.ui
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.aregyan.compose.ui.detail.DetailScreen
 import com.aregyan.compose.ui.user.UserScreen
 
 @Composable
 fun ComposeApp() {
+    val navController = rememberNavController()
     NavHost(
-        navController = rememberNavController(),
+        navController = navController,
         startDestination = Screen.User.route
     ) {
         composable(Screen.User.route) {
             UserScreen(
-                onUserClick = {
-                              
+                onUserClick = { username ->
+                    navController.navigate("${Screen.Detail.route}/$username")
                 }
             )
+        }
+        composable(
+            route = "${Screen.Detail.route}/{${Argument.Username.value}}",
+            arguments = listOf(
+                navArgument(Argument.Username.value) {
+                    type = NavType.StringType
+                }
+            ),
+        ) {
+            DetailScreen()
         }
     }
 }
 
-sealed class Screen(val route: String) {
-    object User : Screen("user")
+enum class Screen(val route: String) {
+    User("user"),
+    Detail("detail")
+}
+
+enum class Argument(val value: String) {
+    Username("username")
 }
