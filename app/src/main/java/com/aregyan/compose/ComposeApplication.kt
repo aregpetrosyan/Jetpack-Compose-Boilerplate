183188ed74d0b50ed7192495c5728c3fa0579aa4
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-06-15 12:11:27 +0400

    Added init Timber

--- app/src/main/java/com/aregyan/compose/ComposeApplication.kt
+++ app/src/main/java/com/aregyan/compose/ComposeApplication.kt
@@ -2,6 +2,16 @@ package com.aregyan.compose
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class ComposeApplication : Application()
+class ComposeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
