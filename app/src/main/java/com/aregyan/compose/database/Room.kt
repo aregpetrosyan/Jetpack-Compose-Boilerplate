9877799a4ecb9227b956df64ff383034146e9fd4
Areg Petrosyan <aregpetrosyan.a@gmail.com>
2022-06-16 16:12:34 +0400

    Added minor improvement

--- app/src/main/java/com/aregyan/compose/database/Room.kt
+++ app/src/main/java/com/aregyan/compose/database/Room.kt
@@ -6,14 +6,12 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface UsersDao {
 
-    // user List
     @Query("select * from UserEntity")
     fun getUsers(): Flow<List<UserEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertUsers(users: List<UserEntity>)
 
-    // single user
     @Query("select * from DetailsEntity WHERE user LIKE :user")
     fun getDetails(user: String): Flow<DetailsEntity>
 
